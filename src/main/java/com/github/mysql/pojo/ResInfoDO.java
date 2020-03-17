b5c6cfd8d942d225b4c7e2f446dbe426d9f64af2
shishaodong <shishaodong@cmhi.chinamobile.com>
2020-03-17 09:54:44 +0800

    额外配置

---
+++ src/main/java/com/github/mysql/pojo/ResInfoDO.java
@@ -0,0 +1,47 @@
+package com.github.mysql.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 创建时间为 上午9:52 2020/3/17
+ * 项目名称 spring-boot-mysql
+ * </p>
+ *
+ * @author 石少东
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+
+@Data
+@Slf4j
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ResInfoDO")
+public class ResInfoDO implements Serializable {
+
+    private static final long serialVersionUID = 5764391990938301268L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long resId;
+
+    private String username;
+
+}
