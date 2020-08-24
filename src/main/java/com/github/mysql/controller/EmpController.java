a1434e9e061676e045f577a590176328aa63c511
shaopro <shaopro@qq.com>
2020-08-24 10:05:45 +0800

    优化代码

--- src/main/java/com/github/mysql/controller/EmpController.java
+++ src/main/java/com/github/mysql/controller/EmpController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,9 +39,11 @@ public class EmpController {
         return repository.save(emp);
     }
 
-    @PutMapping("emp/{empId}")
-    public EmpDO update(@PathVariable("empId") EmpDO emp,@RequestBody EmpDO emp) {
-        return repository.save(emp);
+    @GetMapping("emp/{empId}/{empName}")
+//    @PutMapping("emp/{empId}/{empName}")
+    public EmpDO update(@PathVariable("empId") EmpDO emp, @PathVariable String empName) {
+        emp.setEmpName(empName);
+        return emp;
     }
 
     @DeleteMapping("emp/{empId}")
