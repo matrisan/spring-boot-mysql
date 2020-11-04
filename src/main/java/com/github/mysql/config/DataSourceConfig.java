4dc2d2e63689c4696eb6e34d9dbe30c6c350535c
shaopro <shaopro@qq.com>
2020-11-04 17:03:20 +0800

    多数据源

---
+++ src/main/java/com/github/mysql/config/DataSourceConfig.java
@@ -0,0 +1,31 @@
+package com.github.mysql.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author 石少东
+ * @date 2020-11-04 13:14
+ * @since 1.0
+ */
+
+@Configuration
+public class DataSourceConfig {
+    
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.db1")
+    public DataSource db1DataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.db2")
+    public DataSource db2DataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+}
